4420edfed8670e341bbd5b9830847598ab1f9c51
Umberto Russo <ulver2812@gmail.com>
2019-02-26 10:17:03 +0100

    Removed chrome dev tools

--- main.ts
+++ main.ts
@@ -54,7 +54,7 @@ function createWindow() {
     }));
   }
 
-  win.webContents.openDevTools();
+  // win.webContents.openDevTools();
 
   // Emitted when the window is closed.
   win.on('closed', () => {
